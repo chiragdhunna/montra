eada997887e855a26af9d1cfcb1fdca1d6bbfbbf
chiragdhunna <chiragdhunna2468@gmail.com>
2025-03-11 16:59:41 +0530

    minor changes

---
+++ lib/screens/user screens/financial reports/financial_report_screeen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FinancialReportScreeen extends StatefulWidget {
+  const FinancialReportScreeen({super.key});
+
+  @override
+  State<FinancialReportScreeen> createState() => _FinancialReportScreeenState();
+}
+
+class _FinancialReportScreeenState extends State<FinancialReportScreeen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
