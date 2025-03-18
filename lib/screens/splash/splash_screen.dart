b0c3d85950c2f8f4217bad0cbd864540d98c68ba
chiragdhunna <chiragdhunna2468@gmail.com>
2025-03-19 00:35:47 +0530

    screen loader in splash screen

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:montra/constants/montra_colors.dart';
 import 'package:montra/screens/on_boarding/on_boarding_screen.dart';
+import 'dart:async';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,22 +11,34 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  double _progress = 0.0;
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const OnBoardingScreen()),
-      );
+    _startLoading();
+  }
+
+  void _startLoading() {
+    Timer.periodic(const Duration(milliseconds: 300), (timer) {
+      if (_progress >= 1.0) {
+        timer.cancel();
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const OnBoardingScreen()),
+        );
+      } else {
+        setState(() {
+          _progress = (_progress + 0.1).clamp(0.0, 1.0);
+        });
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-          MontraColors.splashScreenBackgroundColor, // Purple background
+      backgroundColor: MontraColors.splashScreenBackgroundColor,
       body: Stack(
         children: [
           Center(
@@ -42,7 +55,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 ).createShader(bounds);
               },
               blendMode: BlendMode.srcOver,
-              child: Text(
+              child: const Text(
                 'montra',
                 style: TextStyle(
                   fontSize: 40,
@@ -50,6 +63,29 @@ class _SplashScreenState extends State<SplashScreen> {
                   color: Colors.white,
                 ),
               ),
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                LinearProgressIndicator(
+                  value: _progress,
+                  backgroundColor: Colors.white.withOpacity(0.3),
+                  color: Colors.pinkAccent,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  "${(_progress * 100).clamp(0, 100).toInt()}%",
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
