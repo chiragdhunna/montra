1c4bf352bd851c9d6992bc4a50744ea685b7a640
chiragdhunna <chiragdhunna2468@gmail.com>
2025-03-26 20:33:12 +0530

    loader in login screen

--- lib/screens/on_boarding/login_screen.dart
+++ lib/screens/on_boarding/login_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:montra/logic/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:montra/logic/blocs/login_bloc/login_bloc.dart';
 import 'package:montra/screens/on_boarding/forgot_password_screen.dart';
 import 'package:montra/screens/on_boarding/sign_up_screen.dart';
@@ -16,6 +19,85 @@ class _LoginScreenState extends State<LoginScreen> {
   bool _obscurePassword = true;
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool isLoading = false;
+
+  late StreamSubscription<AuthenticationState> authStreamSubscription;
+
+  Future<void> authChangeHandler(AuthenticationState state) async {
+    state.maybeWhen(
+      orElse: () {},
+      initial: () {
+        setState(() {
+          isLoading = false;
+        });
+      },
+      loggedOut: () {
+        setState(() {
+          isLoading = false;
+        });
+      },
+      failure: (error) {
+        setState(() {
+          isLoading = false;
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text('Error $error')));
+        });
+      },
+      inProgress: () {
+        setState(() {
+          isLoading = true;
+        });
+      },
+      authenticated: (user, authToken) {
+        setState(() {
+          isLoading = false;
+        });
+      },
+      unAuthenticated: () {
+        setState(() {
+          isLoading = false;
+        });
+      },
+      userImageUploaded: (user) {
+        setState(() {
+          isLoading = false;
+        });
+      },
+      userLoggedIn: (user, authToken) {
+        setState(() {
+          isLoading = false;
+        });
+      },
+      userSignedUp: () {
+        setState(() {
+          isLoading = false;
+        });
+      },
+      checking: () {
+        setState(() {
+          isLoading = true;
+        });
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    authStreamSubscription = BlocProvider.of<AuthenticationBloc>(
+      context,
+    ).stream.listen(authChangeHandler);
+    authChangeHandler(BlocProvider.of<AuthenticationBloc>(context).state);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    authStreamSubscription.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,131 +122,139 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 40),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                hintText: "Email",
-                filled: true,
-                fillColor: Colors.grey[100],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-            ),
-            const SizedBox(height: 15),
-            TextField(
-              controller: _passwordController,
-              obscureText: _obscurePassword,
-              decoration: InputDecoration(
-                hintText: "Password",
-                filled: true,
-                fillColor: Colors.grey[100],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscurePassword ? Icons.visibility_off : Icons.visibility,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _obscurePassword = !_obscurePassword;
-                    });
-                  },
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (builder) => ForgotPasswordScreen(),
-                    ),
-                  );
-                },
-                child: const Text(
-                  "Forgot Password?",
-                  style: TextStyle(
-                    color: Colors.purple,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_emailController.text.isNotEmpty &&
-                      _passwordController.text.isNotEmpty) {
-                    BlocProvider.of<LoginBloc>(context).add(
-                      LoginEvent.startLogin(
-                        email: _emailController.text,
-                        password: _passwordController.text,
-                      ),
-                    );
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Please fill in all fields"),
-                      ),
-                    );
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.purple,
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                child: const Text(
-                  "Login",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: Text.rich(
-                TextSpan(
-                  text: "Don’t have an account yet? ",
-                  style: const TextStyle(color: Colors.black54),
+      body:
+          isLoading
+              ? Center(child: CircularProgressIndicator())
+              : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TextSpan(
-                      text: "Sign Up",
-                      recognizer:
-                          TapGestureRecognizer()
-                            ..onTap =
-                                () => Navigator.of(context).pushReplacement(
-                                  MaterialPageRoute(
-                                    builder: (builder) => SignUpScreen(),
-                                  ),
-                                ),
-                      style: const TextStyle(
-                        color: Colors.purple,
-                        fontWeight: FontWeight.bold,
+                    const SizedBox(height: 40),
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        filled: true,
+                        fillColor: Colors.grey[100],
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    TextField(
+                      controller: _passwordController,
+                      obscureText: _obscurePassword,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        filled: true,
+                        fillColor: Colors.grey[100],
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _obscurePassword
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _obscurePassword = !_obscurePassword;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (builder) => ForgotPasswordScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            color: Colors.purple,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_emailController.text.isNotEmpty &&
+                              _passwordController.text.isNotEmpty) {
+                            BlocProvider.of<LoginBloc>(context).add(
+                              LoginEvent.startLogin(
+                                email: _emailController.text,
+                                password: _passwordController.text,
+                              ),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("Please fill in all fields"),
+                              ),
+                            );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: Text.rich(
+                        TextSpan(
+                          text: "Don’t have an account yet? ",
+                          style: const TextStyle(color: Colors.black54),
+                          children: [
+                            TextSpan(
+                              text: "Sign Up",
+                              recognizer:
+                                  TapGestureRecognizer()
+                                    ..onTap =
+                                        () => Navigator.of(
+                                          context,
+                                        ).pushReplacement(
+                                          MaterialPageRoute(
+                                            builder:
+                                                (builder) => SignUpScreen(),
+                                          ),
+                                        ),
+                              style: const TextStyle(
+                                color: Colors.purple,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
