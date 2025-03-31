e6e0f96bbd6bb8fec1e15cde993188416fcdb7f8
chiragdhunna <chiragdhunna2468@gmail.com>
2025-03-31 11:26:45 +0530

    adding set of quotes to show in quotesScreen

---
+++ lib/constants/quotes.dart
@@ -0,0 +1,229 @@
+class Quote {
+  final String text;
+  final String author;
+
+  Quote({required this.text, required this.author});
+}
+
+final List<Quote> quotes = [
+  Quote(
+    text: "Financial freedom is freedom from fear.",
+    author: "Robert Kiyosaki",
+  ),
+  Quote(
+    text: "The best way to predict the future is to create it.",
+    author: "Abraham Lincoln",
+  ),
+  Quote(
+    text:
+        "Success is not the key to happiness. Happiness is the key to success.",
+    author: "Albert Schweitzer",
+  ),
+  Quote(
+    text: "Don’t watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  ),
+  Quote(
+    text: "Your time is limited, so don’t waste it living someone else’s life.",
+    author: "Steve Jobs",
+  ),
+  Quote(
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  ),
+  Quote(
+    text: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  ),
+  Quote(
+    text:
+        "The only limit to our realization of tomorrow is our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  ),
+  Quote(
+    text: "It always seems impossible until it’s done.",
+    author: "Nelson Mandela",
+  ),
+  Quote(
+    text: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  ),
+  Quote(
+    text:
+        "Happiness is not something ready made. It comes from your own actions.",
+    author: "Dalai Lama",
+  ),
+  Quote(
+    text: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  ),
+  Quote(
+    text:
+        "Success is not the key to happiness. Happiness is the key to success.",
+    author: "Albert Schweitzer",
+  ),
+  Quote(
+    text: "You miss 100% of the shots you don’t take.",
+    author: "Wayne Gretzky",
+  ),
+  Quote(
+    text:
+        "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  ),
+  Quote(text: "You have to be odd to be number one.", author: "Dr. Seuss"),
+  Quote(
+    text: "Start where you are. Use what you have. Do what you can.",
+    author: "Arthur Ashe",
+  ),
+  Quote(
+    text:
+        "Success usually comes to those who are too busy to be looking for it.",
+    author: "Henry David Thoreau",
+  ),
+  Quote(
+    text: "Don’t let yesterday take up too much of today.",
+    author: "Will Rogers",
+  ),
+  Quote(
+    text: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West",
+  ),
+  Quote(
+    text: "In the middle of every difficulty lies opportunity.",
+    author: "Albert Einstein",
+  ),
+  Quote(
+    text: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  ),
+  Quote(
+    text: "It’s not whether you get knocked down, it’s whether you get up.",
+    author: "Vince Lombardi",
+  ),
+  Quote(
+    text:
+        "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+    author: "Zig Ziglar",
+  ),
+  Quote(
+    text: "It’s hard to beat a person who never gives up.",
+    author: "Babe Ruth",
+  ),
+  Quote(
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  ),
+  Quote(text: "Dream big and dare to fail.", author: "Norman Vaughan"),
+  Quote(text: "Everything you can imagine is real.", author: "Pablo Picasso"),
+  Quote(
+    text: "Do what you can with all you have, wherever you are.",
+    author: "Theodore Roosevelt",
+  ),
+  Quote(
+    text: "A person who never made a mistake never tried anything new.",
+    author: "Albert Einstein",
+  ),
+  Quote(text: "The best revenge is massive success.", author: "Frank Sinatra"),
+  Quote(
+    text: "Don’t wait. The time will never be just right.",
+    author: "Napoleon Hill",
+  ),
+  Quote(
+    text: "Act as if what you do makes a difference. It does.",
+    author: "William James",
+  ),
+  Quote(
+    text: "You are never too old to set another goal or to dream a new dream.",
+    author: "C.S. Lewis",
+  ),
+  Quote(
+    text: "To live a creative life, we must lose our fear of being wrong.",
+    author: "Joseph Chilton Pearce",
+  ),
+  Quote(text: "If you can dream it, you can do it.", author: "Walt Disney"),
+  Quote(
+    text: "Success is the sum of small efforts, repeated day in and day out.",
+    author: "Robert Collier",
+  ),
+  Quote(
+    text:
+        "The only place where success comes before work is in the dictionary.",
+    author: "Vidal Sassoon",
+  ),
+  Quote(
+    text:
+        "I am not a product of my circumstances. I am a product of my decisions.",
+    author: "Stephen Covey",
+  ),
+  Quote(
+    text: "The mind is everything. What you think you become.",
+    author: "Buddha",
+  ),
+  Quote(
+    text: "You must be the change you wish to see in the world.",
+    author: "Mahatma Gandhi",
+  ),
+  Quote(
+    text: "It’s never too late to be what you might have been.",
+    author: "George Eliot",
+  ),
+  Quote(
+    text:
+        "The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well.",
+    author: "Ralph Waldo Emerson",
+  ),
+  Quote(
+    text: "Life isn’t about finding yourself. Life is about creating yourself.",
+    author: "George Bernard Shaw",
+  ),
+  Quote(
+    text:
+        "To live is the rarest thing in the world. Most people exist, that is all.",
+    author: "Oscar Wilde",
+  ),
+  Quote(
+    text: "Success is not in what you have, but who you are.",
+    author: "Bo Bennett",
+  ),
+  Quote(
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  ),
+  Quote(
+    text:
+        "The harder you work for something, the greater you’ll feel when you achieve it.",
+    author: "Anonymous",
+  ),
+  Quote(
+    text: "Do one thing every day that scares you.",
+    author: "Eleanor Roosevelt",
+  ),
+  Quote(
+    text: "We may encounter many defeats but we must not be defeated.",
+    author: "Maya Angelou",
+  ),
+  Quote(
+    text:
+        "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+    author: "Christian D. Larson",
+  ),
+  Quote(
+    text: "It always seems impossible until it’s done.",
+    author: "Nelson Mandela",
+  ),
+  Quote(
+    text:
+        "You are braver than you believe, stronger than you seem, and smarter than you think.",
+    author: "A.A. Milne",
+  ),
+  Quote(
+    text:
+        "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+    author: "Ralph Waldo Emerson",
+  ),
+  Quote(
+    text: "You must do the things you think you cannot do.",
+    author: "Eleanor Roosevelt",
+  ),
+];
